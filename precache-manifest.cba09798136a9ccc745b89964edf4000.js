c63a835e97e8b232100e25e3929a5a2e3a6dfe4e
Kartik <aggarwalkartik96@gmail.com>
2020-08-11 12:40:59 +0530

    Updates

---
+++ precache-manifest.cba09798136a9ccc745b89964edf4000.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef4b55f21ded1d133a388c1b3a4406f",
+    "url": "/dthwebapp/index.html"
+  },
+  {
+    "revision": "fb9abc7f2111846335a1",
+    "url": "/dthwebapp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "10eaa0d4bbaaa9ad02fa",
+    "url": "/dthwebapp/static/css/main.d84fd686.chunk.css"
+  },
+  {
+    "revision": "fb9abc7f2111846335a1",
+    "url": "/dthwebapp/static/js/2.dc1647e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dthwebapp/static/js/2.dc1647e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10eaa0d4bbaaa9ad02fa",
+    "url": "/dthwebapp/static/js/main.994fca71.chunk.js"
+  },
+  {
+    "revision": "a69e82b6eccd1661c1e0",
+    "url": "/dthwebapp/static/js/runtime-main.358ba8dd.js"
+  },
+  {
+    "revision": "1891b84cf1682a1fa65e9322ca441d4e",
+    "url": "/dthwebapp/static/media/pic2.1891b84c.jpg"
+  }
+]);
